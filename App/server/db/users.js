6957c62845246767304911d9988705dca7c6a7eb
BrandenMitchell <71188053+BrandenMitchell@users.noreply.github.com>
2025-10-11 01:21:46 -0500

    Implemented a User model and schema, connected existing user routes for login / register to said model. Connected to front end register and login Page.

--- App/server/db/users.js
+++
@@ -1,6 +0,0 @@
-const userData = [
-  { id: 1, username: "bam", email: "bam@email.com", password: "HelloIsThisWorking1!" },
-  { id: 2, username: "alex", email: "alex@email.com", password: "abcd" }
-];
-
-module.exports = {userData};
