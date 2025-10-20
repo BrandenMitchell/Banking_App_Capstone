b85000afe17abbfde2aee8b56406c64fa38d91aa
sylviatang214 <tnt68k@umsystem.edu>
2025-10-20 17:16:08 -0500

    Update requirements for register page

--- App/client/src/components/register.js
+++ App/client/src/components/register.js
@@ -6,6 +6,103 @@ export default function Register() {
   const [passwordInput, setPasswordInput] = useState("");
   const [rePasswordInput, setRePasswordInput] = useState("");
 
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [shake, setShake] = useState(false);
+
+  // // Password strength check
+  // const getPasswordStrength = (pwd) => {
+  //   if (pwd.length < 8) return "Weak";
+  //   if (pwd.length < 12) return "Medium";
+  //   if (pwd.length >= 15 && /[0-9]/.test(pwd) && /[!@#$%^&*]/.test(pwd)) return "Strong";
+  //   return "Medium";
+  // };
+
+  const validatePassword = (pwd) => {
+    const regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{15,}$/;
+    return regex.test(pwd);
+  };
+
+   const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPasswordInput(value);
+
+    if (!validatePassword(value)) {
+      setPasswordError("Password must be at least 15 characters long and include a number and a special character.");
+    } else {
+      setPasswordError("");
+    }
+
+    // Check if confirm matches
+    if (passwordInput && value !== rePasswordInput) {
+      setConfirmPasswordError("Passwords do not match.");
+    } else {
+      setConfirmPasswordError("");
+    }
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    const value = e.target.value;
+    setRePasswordInput(value);
+
+    if (value !== passwordInput) {
+      setConfirmPasswordError("Passwords do not match.");
+    } else {
+      setConfirmPasswordError("");
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (passwordError || confirmPasswordError || !passwordInput || !rePasswordInput) {
+      setShake(true);
+      setTimeout(() => setShake(false), 500);
+      return;
+    }
+
+    alert("Form successfully submitted!");
+  }
+
+  const isFormValid = !passwordError && !confirmPasswordError && passwordInput && rePasswordInput;
+
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+
+  //   //Password validation
+  //   if (!validatePassword(passwordInput)) {
+  //     setPasswordError("Password must be at least 15 characters long and include a number and a special character.");
+  //     return;
+  //   }else {
+  //     setPasswordError("");
+  //   }
+
+
+  //   // Confirm password validation
+  //   if (passwordInput !== rePasswordInput) {
+  //     setConfirmPasswordError("Passwords do not match.");
+  //     return;
+  //   } else {
+  //     setConfirmPasswordError("");
+  //   }
+
+  //   console.log("test2");
+  //   if (!validateEmail(emailInput)) {
+  //     console.error("/bad email");
+  //     //return;
+  //   }
+
+  //   console.log("Form submitted successfully!");
+  // };
+
   return (
     <div className="login-container">
       <div className="login-card shadow-lg rounded-3">
@@ -16,50 +113,108 @@ export default function Register() {
 
         <div className="space-y-4">
 
-        <label className="block text-gray-700 mb-3">Email</label>
-          <input
-            type="text"
-            style= {{ display: "block", marginBottom: "10px" }}
-            value={emailInput}
-            onChange={(e) => setEmailInput(e.target.value)}
-            placeholder="email"
-            className="form-control"
-          />
-        <label className="block text-gray-700 mb-3">Username</label>
-          <input
-            type="text"
-            style= {{ display: "block", marginBottom: "10px" }}
-            value={usernameInput}
-            onChange={(e) => setUsernameInput(e.target.value)}
-            placeholder="username"
-            className="form-control"
-          />
-          
-          <label className="block text-gray-700 mb-3">Password</label>
-          <input
-            type="text"
-            style= {{ display: "block", marginBottom: "10px" }}
-            value={passwordInput}
-            onChange={(e) => setPasswordInput(e.target.value)}
-            placeholder="password"
-            className="form-control"
-          />
-          <label className="block text-gray-700 mb-3">Retype Password</label>
-          <input
-            type="text"
-            style= {{ display: "block", marginBottom: "10px" }}
-            value={rePasswordInput}
-            onChange={(e) => setRePasswordInput(e.target.value)}
-            placeholder="Retype password"
-            className="form-control"
-          />
-          <div className="form-text small-text">
-              Must be 15+ characters, include a number and special character.
-            </div>
+        <form onSubmit={handleSubmit}>
 
-          <button type="submit" className="btn btn-primary w-100">
-            Register
-          </button>
+          <label className="block text-gray-700 mb-3">Email</label>
+            <input
+              type="text"
+              style= {{ display: "block", marginBottom: "10px" }}
+              value={emailInput}
+              onChange={(e) => setEmailInput(e.target.value)}
+              placeholder="email"
+              className="form-control"
+              required
+            />
+          <label className="block text-gray-700 mb-3">Username</label>
+            <input
+              type="text"
+              style= {{ display: "block", marginBottom: "10px" }}
+              value={usernameInput}
+              onChange={(e) => setUsernameInput(e.target.value)}
+              placeholder="username"
+              className="form-control"
+              required
+            />
+            
+            {/* Password */}
+            <label className="block text-gray-700 mb-3">Password</label>
+            <div className={`relative ${shake ? "animate-shake" : ""}`}>
+            <input
+              type={showPassword ? "text" : "password"}
+              style= {{ display: "block", marginBottom: "10px" }}
+              value={passwordInput}
+              onChange={handlePasswordChange}
+              placeholder="Enter password"
+              className={`w-full p-2 border rounded mb-1 ${
+                passwordError
+                  ? "border-red-500"
+                  : passwordInput
+                  ? "border-green-500"
+                  : "border-gray-300"
+              }`}
+              required
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-2 top-2 text-sm text-gray-600"
+            >
+              {showPassword ? "Hide" : "Show"}
+            </button>
+            </div>
+            {/* {passwordInput && (
+        <p className={`text-sm ${
+          getPasswordStrength(passwordInput) === "Strong"
+            ? "text-green-500"
+            : getPasswordStrength(passwordInput) === "Medium"
+            ? "text-yellow-500"
+            : "text-red-500"
+        }`}>
+          Strength: {getPasswordStrength(passwordInput)}
+        </p>
+      )} */}
+      {passwordError && <p className="text-red-500 font-bold" style={{ color: "red" }}>  
+      {passwordError}</p>}
+
+
+            {/* Retype password*/}
+            <label className="block text-gray-700 mb-3">Retype Password</label>
+            <div className={`relative ${shake ? "animate-shake" : ""}`}>
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              style= {{ display: "block", marginBottom: "10px" }}
+              value={rePasswordInput}
+              onChange={handleConfirmPasswordChange}
+              placeholder="Retype password"
+              className={`w-full p-2 border rounded mb-1 ${
+                confirmPasswordError
+                  ? "border-red-500"
+                  : rePasswordInput
+                  ? "border-green-500"
+                  : "border-gray-300"
+              }`}
+              required
+            />
+            <button
+          type="button"
+          onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+          className="absolute right-2 top-2 text-sm text-gray-600"
+        >
+          {showConfirmPassword ? "Hide" : "Show"}
+        </button>
+      </div>
+            {confirmPasswordError && <p className="text-red-500 font-bold" style={{ color: "red" }}>  
+            {confirmPasswordError}</p>}
+            {!confirmPasswordError && rePasswordInput && (
+            <p className="text-green-500" style={{ color: "green" }}>Passwords matched</p>
+            )}
+
+            <button 
+            type="submit" 
+            className="btn btn-primary w-100">
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </div>
